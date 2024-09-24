55b47d87aca17c3961c43fbb0b22cbb07d6e730a
Max Radin <radin.max@gmail.com>
2024-09-24 17:46:21 -0400

    Removing threshold computation

--- src/qb_gsee_benchmark/qre.py
+++ src/qb_gsee_benchmark/qre.py
@@ -4,128 +4,35 @@
 from typing import Iterable, Optional
 
 import numpy as np
-from openfermion import MolecularData
-from openfermion.resource_estimates import df
-from openfermion.resource_estimates.molecule import cas_to_pyscf, factorized_ccsd_t
-from openfermionpyscf import PyscfMolecularData
-from openfermionpyscf._run_pyscf import compute_integrals
+from openfermion import InteractionOperator
+from openfermion.resource_estimates.molecule import cas_to_pyscf
 from pyLIQTR.BlockEncodings.DoubleFactorized import DoubleFactorized
 from pyLIQTR.ProblemInstances.ChemicalHamiltonian import ChemicalHamiltonian
 from pyLIQTR.qubitization.phase_estimation import QubitizedPhaseEstimation
-from pyscf import ao2mo, gto, scf
+from pyscf import ao2mo
 
 
-def _get_molecular_data(
-    molecule: scf.hf.SCF,
-    mean_field_object: gto.Mole,
-) -> PyscfMolecularData:
-    """Given a PySCF meanfield object and molecule, return a PyscfMolecularData
-    object.
+def integrals2intop(h1, eri, ecore):
+    norb = h1.shape[0]
+    h2_so = np.zeros((2 * norb, 2 * norb, 2 * norb, 2 * norb))
+    h1_so = np.zeros((2 * norb, 2 * norb))
 
-    Returns:
-        A PyscfMolecularData object corresponding to the meanfield object and
-            molecule.
+    # Populate h1_so
+    h1_so[:norb, :norb] = h1
+    h1_so[norb:, norb:] = h1_so[:norb, :norb]
 
-    Raises:
-        SCFConvergenceError: If the SCF calculation does not converge.
-    """
-    molecular_data = MolecularData(
-        geometry=[
-            ("H", (0, 0, 0)),
-        ],  # Dummy geometry needed for naming purposes
-        basis=molecule.basis,
-        multiplicity=molecule.spin + 1,
-        charge=molecule.charge,
+    # Populate h2_so
+    h2_so[0::2, 0::2, 0::2, 0::2] = eri
+    h2_so[1::2, 1::2, 0::2, 0::2] = eri
+    h2_so[0::2, 0::2, 1::2, 1::2] = eri
+    h2_so[1::2, 1::2, 1::2, 1::2] = eri
+
+    # Transpose from 1122 to 1221
+    h2_so = np.transpose(h2_so, (1, 2, 3, 0))
+
+    return InteractionOperator(
+        constant=ecore, one_body_tensor=h1_so, two_body_tensor=h2_so
     )
-
-    molecular_data.n_orbitals = int(molecule.nao)
-    molecular_data.n_qubits = 2 * molecular_data.n_orbitals
-    molecular_data.nuclear_repulsion = float(molecule.energy_nuc())
-
-    molecular_data.hf_energy = float(mean_field_object.e_tot)
-
-    molecular_data._pyscf_data = {  # type: ignore
-        "mol": molecule,
-        "scf": mean_field_object,
-    }
-
-    molecular_data.canonical_orbitals = mean_field_object.mo_coeff.astype(float)
-    molecular_data.orbital_energies = mean_field_object.mo_energy.astype(float)
-
-    one_body_integrals, two_body_integrals = compute_integrals(
-        mean_field_object._eri, mean_field_object
-    )
-    molecular_data.one_body_integrals = one_body_integrals
-    molecular_data.two_body_integrals = two_body_integrals
-    molecular_data.overlap_integrals = mean_field_object.get_ovlp()
-
-    pyscf_molecular_data = PyscfMolecularData.__new__(PyscfMolecularData)
-    pyscf_molecular_data.__dict__.update(molecule.__dict__)
-
-    return molecular_data
-
-
-def choose_double_factorization_threshold(
-    meanfield_object: scf.hf.SCF,
-    eri_full: np.array,
-    candidate_thresholds: Iterable[float],
-    error_budget: float,
-    use_kernel=True,
-    use_triples=True,
-) -> tuple[int, float, float]:
-    """Choose the threshold for double-factorization that satisfies the error budget.
-
-    Note that this function requires that the _eri attribute of the meanfield object be
-        shaped as a four-index tensor.
-
-    Args:
-        meanfield_object: The mean-field object.
-        candidate_thresholds: The candidate thresholds.
-        error_budget: The energy error budget (Ha).
-        use_kernel: Whether to use the kernel.
-        use_triples: Whether to use the triples.
-
-    Returns:
-        The first threshold amongst the candidates that meets the energy error budget
-            as well as the energy error ERI L2 error for this threshold.
-
-    Raises:
-        ValueError: If none of the candidate thresholds meets the energy error budget.
-    """
-
-    escf, ecor, etot = factorized_ccsd_t(
-        meanfield_object, eri_rr=None, use_kernel=use_kernel, no_triples=not use_triples
-    )
-
-    exact_etot = etot
-
-    best_threshold: Optional[int] = None
-    for threshold in candidate_thresholds:
-        eri_rr, LR, L, Lxi = df.factorize(eri_full, threshold)
-
-        try:
-            escf, ecor, etot = factorized_ccsd_t(
-                meanfield_object,
-                eri_rr,
-                use_kernel=use_kernel,
-                no_triples=not use_triples,
-            )
-        except Exception as e:
-            print(f"Threshold {threshold} failed: {e}")
-
-        error = etot - exact_etot
-        l2_norm_error_eri = np.linalg.norm(
-            eri_rr - eri_full
-        )  # eri reconstruction error
-
-        print(
-            f"Threshold: {threshold}, error: {error}, eri L2 error: {l2_norm_error_eri}"
-        )
-
-        if np.abs(error) < error_budget:
-            return threshold, error, l2_norm_error_eri
-
-    raise ValueError("None of the thresholds provided satisfies the error budget.")
 
 
 def get_num_shots(square_overlap: float, failure_tolerance: float):
@@ -164,7 +71,7 @@ def get_df_qpe_circuit(
     square_overlap: float,
     error_tolerance: float,
     failure_tolerance: float,
-    candidate_thresholds: Optional[Iterable[float]] = None,
+    df_threshold: float,
 ):
     """Get the QPE circuit for a given PySCF FCI object.
 
@@ -184,9 +91,6 @@ def get_df_qpe_circuit(
         A tuple containing the QPE circuit, the number of shots, and the allowable
             hardware failure rate per shot.
     """
-    if candidate_thresholds is None:
-        candidate_thresholds = np.power(10.0, np.arange(-1, -6, -0.5))
-
     # Allowed probability that spectral leakage causes the estimated energy to deviate
     # from the true eigenvalue of the encoded Hamiltonian by more than
     # phase_estimation_error_tolerance. Corresponds to \overline{\delta_{QPE}} in
@@ -218,30 +122,8 @@ def get_df_qpe_circuit(
 
     num_shots = get_num_shots(square_overlap, state_projection_failure_tolerance)
 
-    num_alpha = (fci["NELEC"] + fci["MS2"]) // 2
-    num_beta = num_alpha - fci["MS2"]
-    eri_full = ao2mo.restore("s1", fci["H2"], fci["H1"].shape[0])
-
-    active_space_molecule, active_space_meanfield_object = cas_to_pyscf(
-        fci["H1"], eri_full, fci["ECORE"], num_alpha, num_beta
-    )
-
-    (
-        df_threshold,
-        df_energy_error,
-        df_eri_l2_error,
-    ) = choose_double_factorization_threshold(
-        meanfield_object=active_space_meanfield_object,
-        eri_full=eri_full,
-        candidate_thresholds=candidate_thresholds,
-        error_budget=block_encoding_error_tolerance / 3,
-    )
-
-    molecular_data = _get_molecular_data(
-        active_space_molecule, active_space_meanfield_object
-    )
-
-    interaction_op = molecular_data.get_molecular_hamiltonian()
+    eri = ao2mo.restore("s1", fci["H2"], fci["NORB"])
+    interaction_op = integrals2intop(h1=fci["H1"], eri=eri, ecore=fci["ECORE"])
     instance = ChemicalHamiltonian(mol_ham=interaction_op, mol_name="H2")
 
     # Note that the factor of ten arises because because PyLIQTR will multiply by 0.1 to
@@ -251,7 +133,7 @@ def get_df_qpe_circuit(
         instance,
         energy_error=block_encoding_error_tolerance * 10 * 2 / 3,
         df_error_threshold=df_threshold,
-        sf_error_threshold=1e-12,
+        sf_error_threshold=1e-12,  # See https://github.com/isi-usc-edu/pyLIQTR/issues/21
     )
 
     circuit = QubitizedPhaseEstimation(encoding, eps=sigma)
