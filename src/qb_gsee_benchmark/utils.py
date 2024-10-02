1631a265275abcfcd1f52ffcf998d09144beb091
Max Radin <radin.max@gmail.com>
2024-10-02 15:33:09 -0400

    Adding fixed mn mono QRE

--- src/qb_gsee_benchmark/utils.py
+++ src/qb_gsee_benchmark/utils.py
@@ -11,7 +11,7 @@ import paramiko
 from pyscf.tools import fcidump
 
 
-def fetch_file_from_sftp(
+def _fetch_file_from_sftp(
     url: str, local_path: str, ppk_path: str, username: str, port=22
 ):
 
@@ -29,14 +29,15 @@ def fetch_file_from_sftp(
         )
 
         with client.open_sftp() as sftp:
+            print(f"Downloading {remote_path} to {local_path}...")
             sftp.get(remote_path, local_path)
 
 
 def retrieve_fcidump_from_sftp(url: str, username: str, ppk_path: str, port=22) -> dict:
     filename = os.path.basename(urlparse(url).path)
-    fetch_file_from_sftp(
-        url=url, username=username, ppk_path=ppk_path, local_path=filename, port=port
-    )
+    # _fetch_file_from_sftp(
+    #     url=url, username=username, ppk_path=ppk_path, local_path=filename, port=port
+    # )
     fcidump_filename = filename.replace(".gz", "")
     with gzip.open(filename, "rb") as f_in:
         with open(fcidump_filename, "wb") as f_out:
--- tests/qb_gsee_benchmark/test_qre.py
+++ tests/qb_gsee_benchmark/test_qre.py
@@ -3,24 +3,62 @@
 ################################################################################
 
 
+import numpy as np
+import pytest
 from pyscf.tools import fcidump
+import os
 
-from qb_gsee_benchmark.qre import get_df_qpe_circuit, get_num_shots
+from qb_gsee_benchmark.qre import (
+    get_df_qpe_circuit,
+    get_failure_tolerance_per_shot,
+    get_num_shots,
+)
 
 
-def test_get_num_shots():
-    assert get_num_shots(square_overlap=0.9, failure_tolerance=0.1) == 1
-
-
-def get_hardware_failure_tolerance_per_shot():
+@pytest.mark.parametrize(
+    "square_overlap, failure_tolerance, expected_num_shots",
+    [
+        (0.9, 0.1, 1),
+        (0.8, 0.1, 2),
+    ],
+)
+def test_get_num_shots(square_overlap, failure_tolerance, expected_num_shots):
     assert (
-        get_hardware_failure_tolerance_per_shot(failure_tolerance=0.1, num_shots=1)
-        == 0.1
+        get_num_shots(
+            square_overlap=square_overlap, failure_tolerance=failure_tolerance
+        )
+        == expected_num_shots
     )
 
 
-# def test_get_df_qpe_circuit():
-#     fci = fcidump.read(filename="h2_6-31g.FCIDUMP")
-#     circuit, n_shots, allowable_error = get_df_qpe_circuit(
-#         fci=fci, target_accuracy=1e-3, allowabel_failure_rate=1e-2
-#     )
+@pytest.mark.parametrize(
+    "failure_tolerance, num_shots, expected_failure_tolerance_per_shot",
+    [
+        (0.1, 1, 0.1),
+        (7 / 16, 2, 1 / 4),
+    ],
+)
+def test_get_failure_tolerance_per_shot(
+    failure_tolerance, num_shots, expected_failure_tolerance_per_shot
+):
+    assert np.isclose(
+        get_failure_tolerance_per_shot(
+            failure_tolerance=failure_tolerance, num_shots=num_shots
+        ),
+        expected_failure_tolerance_per_shot,
+    )
+
+
+def test_get_df_qpe_circuit():
+    fci = fcidump.read(filename=os.path.join(os.path.dirname(__file__), "FCIDUMP_b_sto-3g"))
+    square_overlap = 0.8
+    failure_tolerance = 1e-2
+    error_tolerance = 1.6e-3
+    circuit, num_shots, allowable_error = get_df_qpe_circuit(
+        fci=fci,
+        square_overlap=square_overlap,
+        error_tolerance=error_tolerance,
+        failure_tolerance=failure_tolerance,
+        df_threshold=1e-3
+    )
+    assert (1 - square_overlap) ** num_shots < failure_tolerance
